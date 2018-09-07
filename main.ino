583cbd2a786f4d9a9349cb021176ef3d66bc3699
Fabio Fernandes <fabio_sfernandes@hotmail.com>
2018-09-07 11:55:51 -0300

    initial files

---
+++ main.ino
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include "service.h"
+
+wifi_config *wifi;
+
+void setup() {
+    Serial.begin(115200);
+
+    wifi = new wifi_config(false); 
+    wifi->SetConfig();
+
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
